47fd95623638a02abcb8c353fe0118383c2ebf89
DarkStar <8171397+ui-DarkStar@users.noreply.github.com>
2021-09-10 19:22:12 +0100

    Initial commit

---
+++ src/pontoon/start.java
@@ -0,0 +1,16 @@
+package pontoon;
+
+public class start {
+
+	public static void main(String[] args) {
+		int value=0;
+		for (int x=0;x<10;x++)
+		{
+			value = (int)(Math.random()*10) +1;
+			System.out.print(value+" ");
+		}
+
+	}
+
+}
+fff
